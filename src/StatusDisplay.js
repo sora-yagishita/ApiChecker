b1ba6fb20702ced14003cb0d50a734ea4a9fe080
sora-yagishita <ygst3.14159@gmail.com>
2023-11-08 01:55:05 +0900

    Param設定を可能にする＆API毎に実行時間指定可能にする

--- src/StatusDisplay.js
+++ src/StatusDisplay.js
@@ -1,19 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function ConvertJson(api) {
-  const apiParams = {};
-  api.apiParams.forEach((param) => {
-    apiParams[param.key] = param.value;
-  });
-  console.log(apiParams);
-  return apiParams;
-}
-
 function StatusDisplay() {
   const [apiInfo, setApiInfo] = useState([]);
   const [statusCodes, setStatusCodes] = useState({});
-  const [lastApiRequestTime, setLastApiRequestTime] = useState(null);
+  const [lastApiRequestTime, setLastApiRequestTime] = useState({});
 
   useEffect(() => {
     const endpointData = require('./endpoints.json');
@@ -23,40 +14,46 @@ function StatusDisplay() {
 
     apiInfo.forEach((api) => {
       fetchStatusCode(api);
+      setInterval(() => fetchStatusCode(api), api.requestIntervalMin * 60000);
     });
 
-    const intervalId = setInterval(() => {
+    return () => {
       apiInfo.forEach((api) => {
-        fetchStatusCode(api);
+        clearInterval(api.intervalId);
       });
-    }, 3600000);
-
-    return () => clearInterval(intervalId);
+    }
   }, []);
 
   const fetchStatusCode = async (api) => {
-    const params = ConvertJson(api);
+    const params = convertJson(api);
     await axios.get(
       api.apiEndpoint,
-      {
-        params: {
-          params
-        }
-      }
+      {params}
     ).then(response => {
-      setStatusCodes((prevStatusCodes) => ({
-        ...prevStatusCodes,
-        [api.apiName]: response.status,
-      }));
-      setLastApiRequestTime(new Date());
+      setStatus(api.apiName, response.status);
     }).catch(error => {
-      console.error(`${api.apiName} API呼び出し中にエラーが発生しました`, error);
-      setStatusCodes((prevStatusCodes) => ({
-        ...prevStatusCodes,
-        [api.apiName]: error.response.status,
-      }));
+      setStatus(api.apiName, error.response.status);
     })
   };
+
+  const convertJson = (api) => {
+    const apiParams = {};
+    api.apiParams.forEach((param) => {
+      apiParams[param.key] = param.value;
+    });
+    return apiParams;
+  }
+
+  const setStatus = (apiName, status) => {
+    setStatusCodes((prevStatusCodes) => ({
+      ...prevStatusCodes,
+      [apiName]: status,
+    }));
+    setLastApiRequestTime((lastApiRequestTime) => ({
+      ...lastApiRequestTime,
+      [apiName]: new Date(),
+    }));
+  }
 
   return (
     <div>
@@ -64,11 +61,19 @@ function StatusDisplay() {
         <div key={api.apiName}>
           <p>API名: {api.apiName}</p>
           <p>ステータスコード: {statusCodes[api.apiName]}</p>
-          <p>最終更新時間: {lastApiRequestTime ? lastApiRequestTime.toLocaleTimeString() : 'まだ送信されていません'}</p>
+          <p>最終更新時間: {lastApiRequestTime[api.apiName] ? lastApiRequestTime[api.apiName].toLocaleTimeString() : 'まだ送信されていません'}</p>
         </div>
       ))}
     </div>
   );
+}
+
+function SetStatus(api) {
+  const apiParams = {};
+  api.apiParams.forEach((param) => {
+    apiParams[param.key] = param.value;
+  });
+  return apiParams;
 }
 
 export default StatusDisplay;
